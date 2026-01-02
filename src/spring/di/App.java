7503d1fad8ddae22bc1cfed23411ebdde5f28915
GI-HYUK KWON <7lhyuk@gmail.com>
2026-01-02 16:18:10 +0900

    FEAT: DI(by using xml)

--- src/spring/di/App.java
+++ src/spring/di/App.java
@@ -1,6 +1,5 @@
 package spring.di;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.context.ApplicationContext;
@@ -8,21 +7,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import spring.di.ui.ExamConsole;
 import spring.di.entity.Exam;
-import spring.di.entity.NewExam;
 
 
 public class App {
     public static void main(String[] args) {
         ApplicationContext context = 
-            new ClassPathXmlApplicationContext("spring/di/setting.xml");
+            new ClassPathXmlApplicationContext("spring/di/applicationContext.xml");
         
         ExamConsole console = context.getBean(ExamConsole.class);
         // ExamConsole console = (ExamConsole) context.getBean("examConsole");
         console.print();
 
-        List<Exam> exams = new ArrayList<>();
-        exams.add(new NewExam(1, 1, 1, 1));
-
+        List<Exam> exams = (List<Exam>) context.getBean("exams");
         for (Exam e: exams)
             System.out.println(e);
     }
