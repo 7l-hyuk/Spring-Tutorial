34000c388ee333e297e8831b4a92e43fdb45d4cf
GI-HYUK KWON <7lhyuk@gmail.com>
2026-01-02 18:55:07 +0900

    FEAT: add AOP by using spring xml config

--- src/spring/aop/Program.java
+++ src/spring/aop/Program.java
@@ -1,39 +1,17 @@
 package spring.aop;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import spring.aop.entity.Exam;
-import spring.aop.entity.NewExam;
 
 
 public class Program {
     public static void main(String[] args) {
-        Exam exam = new NewExam(1, 1, 1, 1);
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring/aop/applicationContext.xml");
+        Exam exam = (Exam) context.getBean("exam");
 
-        Exam proxy = (Exam) Proxy.newProxyInstance(
-            NewExam.class.getClassLoader(),
-            new Class[] {Exam.class},
-            new InvocationHandler() {
-                @Override
-                public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                    // cross-cutting consern
-                    long start = System.currentTimeMillis();
-                    
-                    // core consern
-                    Object result = method.invoke(exam, args);
-                    
-                    // cross-cutting consern
-                    long end = System.currentTimeMillis();
-                    String message = (end - start) + "ms";
-                    System.out.println(message);
-                    return result;
-                }
-            }
-        );
-
-        System.out.printf("total is %d%n", proxy.total());
-        System.out.printf("avg is %f%n", proxy.avg());
+        System.out.printf("total is %d%n", exam.total());
+        System.out.printf("avg is %f%n", exam.avg());
     }
 }
